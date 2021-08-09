dc4fe270314e76aec402b6fba72944ea7291ff59
anna-left <anna_left@mail.ru>
2021-08-09 19:04:15 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let nStr = String(Math.abs(n));
+    let res = '';
+    for (var i = 0; i < nStr.length; i++) {
+        res = nStr[i] + res;
+    }
+    return Number(res);
 }
